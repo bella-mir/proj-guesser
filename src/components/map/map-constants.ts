040d29a13ee3adfedba568e8002cf206a75752f5
Bella Mironova <bellamironova@gmail.com>
2024-03-19 07:53:05 +0100

    add: add specifications for all projections

---
+++ src/components/map/map-constants.ts
@@ -0,0 +1,670 @@
+import * as d3 from "d3";
+//@ts-ignore
+import * as d3proj from "d3-geo-projection";
+
+export const PROJECTIONS = [
+  {
+    name: "Azimuthal Equal Area",
+    function: d3.geoAzimuthalEqualArea(),
+    type: "Azimuthal",
+    property: "Equal-area",
+  },
+  {
+    name: "Azimuthal Equidistant",
+    function: d3.geoAzimuthalEquidistant(),
+    type: "Azimuthal",
+    property: "Equidistant",
+  },
+  {
+    name: "Gnomonic",
+    function: d3.geoGnomonic(),
+    type: "Azimuthal",
+    property: "Gnomonic",
+  },
+  {
+    name: "Orthographic",
+    function: d3.geoOrthographic(),
+    type: "Azimuthal",
+    property: "Perspective",
+  },
+  {
+    name: "Stereographic",
+    function: d3.geoStereographic(),
+    type: "Azimuthal",
+    property: "Conformal",
+  },
+  {
+    name: "Equal Earth",
+    function: d3.geoEqualEarth(),
+    type: "Pseudocylindrical",
+    property: "Equal-area",
+  },
+  {
+    name: "Albers Usa",
+    function: d3.geoAlbersUsa(),
+    type: "Conic",
+    property: "Equal-area",
+  },
+  {
+    name: "Albers",
+    function: d3.geoAlbers(),
+    type: "Conic",
+    property: "Equal-area",
+  },
+  {
+    name: "Conic Equal Area",
+    function: d3.geoConicEqualArea(),
+    type: "Conic",
+    property: "Equal-area",
+  },
+  {
+    name: "Conic Equidistant",
+    function: d3.geoConicEquidistant(),
+    type: "Conic",
+    property: "Equidistant",
+  },
+  {
+    name: "Equirectangular",
+    function: d3.geoEquirectangular(),
+    type: "Cylindrical",
+    property: "Equidistant",
+  },
+  {
+    name: "Mercator",
+    function: d3.geoMercator(),
+    type: "Cylindrical",
+    property: "Conformal",
+  },
+  {
+    name: "Transverse Mercator",
+    function: d3.geoTransverseMercator(),
+    type: "Cylindrical",
+    property: "Conformal",
+  },
+  {
+    name: "Natural Earth1",
+    function: d3.geoNaturalEarth1(),
+    type: "Pseudocylindrical",
+    property: "Compromise",
+  },
+  {
+    name: "Airy",
+    function: d3proj.geoAiry(),
+    type: "Azimuthal",
+    property: "Compromise",
+  },
+  {
+    name: "Aitoff",
+    function: d3proj.geoAitoff(),
+    type: "Pseudoazimuthal",
+    property: "Compromise",
+  },
+  {
+    name: "Armadillo",
+    function: d3proj.geoArmadillo(),
+    type: "Other",
+    property: "Compromise",
+  },
+  {
+    name: "August",
+    function: d3proj.geoAugust(),
+    type: "Other",
+    property: "Conformal",
+  },
+  {
+    name: "Baker",
+    function: d3proj.geoBaker(),
+    type: "Pseudocylindrical",
+    property: "Compromise",
+  },
+  {
+    name: "Berghaus",
+    function: d3proj.geoBerghaus(),
+    type: "Azimuthal",
+    property: "Equidistant",
+  },
+  {
+    name: "Bertin1953",
+    function: d3proj.geoBertin1953(),
+    type: "Other",
+    property: "Compromise",
+  },
+  {
+    name: "Boggs",
+    function: d3proj.geoBoggs(),
+    type: "Pseudocylindrical",
+    property: "Equal-area",
+  },
+  {
+    name: "Bonne",
+    function: d3proj.geoBonne(),
+    type: "Pseudoconical",
+    property: "Equal-area",
+  },
+  {
+    name: "Bottomley",
+    function: d3proj.geoBottomley(),
+    type: "Pseudoconical",
+    property: "Equal-area",
+  },
+  {
+    name: "Bromley",
+    function: d3proj.geoBromley(),
+    type: "Pseudocylindrical",
+    property: "Equal-area",
+  },
+  {
+    name: "Chamberlin Africa",
+    function: d3proj.geoChamberlinAfrica(),
+    type: "Trimetric",
+    property: "Other",
+  },
+  {
+    name: "Collignon",
+    function: d3proj.geoCollignon(),
+    type: "Pseudocylindrical",
+    property: "Equal-area",
+  },
+  {
+    name: "Craig",
+    function: d3proj.geoCraig(),
+    type: "Retroazimuthal",
+    property: "Compromise",
+  },
+  {
+    name: "Craster",
+    function: d3proj.geoCraster(),
+    type: "Pseudocylindrical",
+    property: "Equal-area",
+  },
+  {
+    name: "CylindricalEqualArea",
+    function: d3proj.geoCylindricalEqualArea(),
+    type: "Cylindrical",
+    property: "Equal-area",
+  },
+  {
+    name: "CylindricalStereographic",
+    function: d3proj.geoCylindricalStereographic(),
+    type: "Cylindrical",
+    property: "Conformal",
+  },
+  {
+    name: "Eckert1",
+    function: d3proj.geoEckert1(),
+    type: "Pseudocylindrical",
+    property: "Equal-area",
+  },
+  {
+    name: "Eckert2",
+    function: d3proj.geoEckert2(),
+    type: "Pseudocylindrical",
+    property: "Equal-area",
+  },
+  {
+    name: "Eckert3",
+    function: d3proj.geoEckert3(),
+    type: "Pseudocylindrical",
+    property: "Equal-area",
+  },
+  {
+    name: "Eckert4",
+    function: d3proj.geoEckert4(),
+    type: "Pseudocylindrical",
+    property: "Equal-area",
+  },
+  {
+    name: "Eckert5",
+    function: d3proj.geoEckert5(),
+    type: "Pseudocylindrical",
+    property: "Equal-area",
+  },
+  {
+    name: "Eckert6",
+    function: d3proj.geoEckert6(),
+    type: "Pseudocylindrical",
+    property: "Equal-area",
+  },
+  {
+    name: "Eisenlohr",
+    function: d3proj.geoEisenlohr(),
+    type: "Other",
+    property: "Conformal",
+  },
+  {
+    name: "Fahey",
+    function: d3proj.geoFahey(),
+    type: "Pseudocylindrical",
+    property: "Other",
+  },
+  {
+    name: "Foucaut",
+    function: d3proj.geoFoucaut(),
+    type: "Pseudocylindrical",
+    property: "Equal-area",
+  },
+  {
+    name: "Foucaut Sinusoidal",
+    function: d3proj.geoFoucautSinusoidal(),
+    type: "Pseudocylindrical",
+    property: "Equal-area",
+  },
+  {
+    name: "Gilbert",
+    function: d3proj.geoGilbert(),
+    type: "Azimuthal",
+    property: "Conformal",
+  },
+  {
+    name: "Gingery",
+    function: d3proj.geoGingery(),
+    type: "Azimuthal",
+    property: "Equidistant",
+  },
+  {
+    name: "Ginzburg4",
+    function: d3proj.geoGinzburg4(),
+    type: "Lenticular",
+    property: "Compromise",
+  },
+  {
+    name: "Ginzburg5",
+    function: d3proj.geoGinzburg5(),
+    type: "Lenticular",
+    property: "Compromise",
+  },
+  {
+    name: "Ginzburg6",
+    function: d3proj.geoGinzburg6(),
+    type: "Lenticular",
+    property: "Compromise",
+  },
+  {
+    name: "Ginzburg8",
+    function: d3proj.geoGinzburg8(),
+    type: "Lenticular",
+    property: "Compromise",
+  },
+  {
+    name: "Ginzburg9",
+    function: d3proj.geoGinzburg9(),
+    type: "Lenticular",
+    property: "Compromise",
+  },
+  {
+    name: "Gringorten",
+    function: d3proj.geoGringorten(),
+    type: "Other",
+    property: "Equal-area",
+  },
+  {
+    name: "Guyou",
+    function: d3proj.geoGuyou(),
+    type: "Other",
+    property: "Conformal",
+  },
+  {
+    name: "Hammer",
+    function: d3proj.geoHammer(),
+    type: "Pseudoazimuthal",
+    property: "Equal-area",
+  },
+  {
+    name: "Hammer Retroazimuthal",
+    function: d3proj.geoHammerRetroazimuthal(),
+    type: "Retroazimuthal",
+    property: "Other",
+  },
+  {
+    name: "Healpix",
+    function: d3proj.geoHealpix(),
+    type: "Pseudocylindrical",
+    property: "Equal-area",
+  },
+  {
+    name: "Hill",
+    function: d3proj.geoHill(),
+    type: "Pseudoconical",
+    property: "Equal-area",
+  },
+  {
+    name: "Homolosine",
+    function: d3proj.geoHomolosine(),
+    type: "Pseudocylindrical",
+    property: "Equal-area",
+  },
+  {
+    name: "Hufnagel",
+    function: d3proj.geoHufnagel(),
+    type: "Pseudocylindrical",
+    property: "Equal-area",
+  },
+  {
+    name: "Hyperelliptical",
+    function: d3proj.geoHyperelliptical(),
+    type: "Pseudocylindrical",
+    property: "Equal-area",
+  },
+  {
+    name: "Kavrayskiy7",
+    function: d3proj.geoKavrayskiy7(),
+    type: "Pseudocylindrical",
+    property: "Compromise",
+  },
+  {
+    name: "Lagrange",
+    function: d3proj.geoLagrange(),
+    type: "Other",
+    property: "Conformal",
+  },
+  {
+    name: "Larrivee",
+    function: d3proj.geoLarrivee(),
+    type: "Other",
+    property: "Compromise",
+  },
+  {
+    name: "Laskowski",
+    function: d3proj.geoLaskowski(),
+    type: "Other",
+    property: "Compromise",
+  },
+  {
+    name: "Loximuthal",
+    function: d3proj.geoLoximuthal(),
+    type: "Pseudocylindrical",
+    property: "Compromise",
+  },
+  {
+    name: "Miller",
+    function: d3proj.geoMiller(),
+    type: "Cylindrical",
+    property: "Compromise",
+  },
+  {
+    name: "Modified Stereographic Alaska",
+    function: d3proj.geoModifiedStereographicAlaska(),
+    type: "Azimuthal",
+    property: "Conformal",
+  },
+  {
+    name: "Modified Stereographic Gs48",
+    function: d3proj.geoModifiedStereographicGs48(),
+    type: "Azimuthal",
+    property: "Conformal",
+  },
+  {
+    name: "Modified Stereographic Gs50",
+    function: d3proj.geoModifiedStereographicGs50(),
+    type: "Azimuthal",
+    property: "Conformal",
+  },
+  {
+    name: "Modified Stereographic Miller",
+    function: d3proj.geoModifiedStereographicMiller(),
+    type: "Azimuthal",
+    property: "Conformal",
+  },
+  {
+    name: "Modified Stereographic Lee",
+    function: d3proj.geoModifiedStereographicLee(),
+    type: "Azimuthal",
+    property: "Conformal",
+  },
+  {
+    name: "Mollweide",
+    function: d3proj.geoMollweide(),
+    type: "Pseudocylindrical",
+    property: "Equal-area",
+  },
+  {
+    name: "Mt Flat Polar Parabolic",
+    function: d3proj.geoMtFlatPolarParabolic(),
+    type: "Pseudocylindrical",
+    property: "Equal-area",
+  },
+  {
+    name: "Mt Flat Polar Quartic",
+    function: d3proj.geoMtFlatPolarQuartic(),
+    type: "Pseudocylindrical",
+    property: "Equal-area",
+  },
+  {
+    name: "Mt Flat Polar Sinusoidal",
+    function: d3proj.geoMtFlatPolarSinusoidal(),
+    type: "Pseudocylindrical",
+    property: "Equal-area",
+  },
+  {
+    name: "Natural Earth 2",
+    function: d3proj.geoNaturalEarth2(),
+    type: "Pseudocylindrical",
+    property: "Compromise",
+  },
+  {
+    name: "Nell Hammer",
+    function: d3proj.geoNellHammer(),
+    type: "Pseudocylindrical",
+    property: "Equal-area",
+  },
+  {
+    name: "Nicolosi",
+    function: d3proj.geoNicolosi(),
+    type: "Pseudoconical",
+    property: "Compromise",
+  },
+  {
+    name: "Patterson",
+    function: d3proj.geoPatterson(),
+    type: "Cylindrical",
+    property: "Compromise",
+  },
+  {
+    name: "Polyconic",
+    function: d3proj.geoPolyconic(),
+    type: "Polyconic",
+    property: "Other",
+  },
+  {
+    name: "Rectangular Polyconic",
+    function: d3proj.geoRectangularPolyconic(),
+    type: "Polyconic",
+    property: "Other",
+  },
+  {
+    name: "Robinson",
+    function: d3proj.geoRobinson(),
+    type: "Pseudocylindrical",
+    property: "Compromise",
+  },
+  {
+    name: "Satellite",
+    function: d3proj.geoSatellite(),
+    type: "Other",
+    property: "Other",
+  },
+  {
+    name: "Sinusoidal",
+    function: d3proj.geoSinusoidal(),
+    type: "Pseudocylindrical",
+    property: "Equal-area",
+  },
+  {
+    name: "Sinu Mollweide",
+    function: d3proj.geoSinuMollweide(),
+    type: "Pseudocylindrical",
+    property: "Equal-area",
+  },
+  {
+    name: "Times",
+    function: d3proj.geoTimes(),
+    type: "Pseudocylindrical",
+    property: "Compromise",
+  },
+  {
+    name: "Two Point Azimuthal Usa",
+    function: d3proj.geoTwoPointAzimuthalUsa(),
+    type: "Azimuthal",
+    property: "Equidistant",
+  },
+  {
+    name: "Van Der Grinten 2",
+    function: d3proj.geoVanDerGrinten2(),
+    type: "Other",
+    property: "Compromise",
+  },
+  {
+    name: "Van Der Grinten 3",
+    function: d3proj.geoVanDerGrinten3(),
+    type: "Other",
+    property: "Compromise",
+  },
+  {
+    name: "Wagner",
+    function: d3proj.geoWagner(),
+    type: "Azimuthal",
+    property: "Equidistant",
+  },
+  {
+    name: "Wagner4",
+    function: d3proj.geoWagner4(),
+    type: "Pseudocylindrical",
+    property: "Compromise",
+  },
+  {
+    name: "Wagner6",
+    function: d3proj.geoWagner6(),
+    type: "Pseudocylindrical",
+    property: "Compromise",
+  },
+  {
+    name: "Wagner7",
+    function: d3proj.geoWagner7(),
+    type: "Pseudoazimuthal",
+    property: "Equal-area",
+  },
+  {
+    name: "Winkel 3",
+    function: d3proj.geoWinkel3(),
+    type: "Pseudoazimuthal",
+    property: "Compromise",
+  },
+
+  {
+    name: "Interrupted Homolosine",
+    function: d3proj.geoInterruptedHomolosine(),
+    type: "Pseudocylindrical",
+    property: "Equal-area",
+  },
+  {
+    name: "Interrupted Sinusoidal",
+    function: d3proj.geoInterruptedSinusoidal(),
+    type: "Pseudocylindrical",
+    property: "Equal-area",
+  },
+  {
+    name: "Interrupted Mollweide",
+    function: d3proj.geoInterruptedMollweide(),
+    type: "Pseudocylindrical",
+    property: "Equidistant",
+  },
+  {
+    name: "Interrupted Sinu Mollweide",
+    function: d3proj.geoInterruptedSinuMollweide(),
+    type: "Pseudocylindrical",
+    property: "Equal-area",
+  },
+  {
+    name: "Interrupted Mollweide Hemispheres",
+    function: d3proj.geoInterruptedMollweideHemispheres(),
+    type: "Pseudocylindrical",
+    property: "Equal-area",
+  },
+  {
+    name: "Polyhedral Butterfly",
+    function: d3proj.geoPolyhedralButterfly(),
+    type: "Polyhedral",
+    property: "Equidistant",
+  },
+  {
+    name: "Polyhedral Collignon",
+    function: d3proj.geoPolyhedralCollignon(),
+    type: "Polyhedral",
+    property: "Equidistant",
+  },
+  {
+    name: "Polyhedral Waterman",
+    function: d3proj.geoPolyhedralWaterman(),
+    type: "Polyhedral",
+    property: "Equidistant",
+  },
+  {
+    name: "Peirce Quincuncial",
+    function: d3proj.geoPeirceQuincuncial(),
+    type: "Quincuncial",
+    property: "Conformal",
+  },
+];
+
+//   , s0 = [{
+//     name: "Azimuthal",
+//     description: "In standard presentation, azimuthal projections map meridians as straight lines and parallels as complete, concentric circles. They are radially symmetrical. In any presentation (or aspect), they preserve directions from the center point. This means great circles through the central point are represented by straight lines on the map."
+// }, {
+//     name: "Cylindrical",
+//     description: "In standard presentation, these map regularly-spaced meridians to equally spaced vertical lines, and parallels to horizontal lines."
+// }, {
+//     name: "Pseudocylindrical",
+//     description: "In standard presentation, these map the central meridian and parallels as straight lines. Other meridians are curves (or possibly straight from pole to equator), regularly spaced along parallels."
+// }, {
+//     name: "Conic",
+//     description: "In standard presentation, conic (or conical) projections map meridians as straight lines, and parallels as arcs of circles."
+// }, {
+//     name: "Pseudoconical",
+//     description: "In standard presentation, pseudoconical projections represent the central meridian as a straight line, other meridians as complex curves, and parallels as circular arcs."
+// }, {
+//     name: "Pseudoazimuthal",
+//     description: "In standard presentation, pseudoazimuthal projections map the equator and central meridian to perpendicular, intersecting straight lines. They map parallels to complex curves bowing away from the equator, and meridians to complex curves bowing in toward the central meridian. Listed here after pseudocylindrical as generally similar to them in shape and purpose."
+// }, {
+//     name: "Other",
+//     description: "Typically calculated from formula, and not based on a particular projection."
+// }, {
+//     name: "Polyhedral",
+//     description: "Polyhedral maps can be folded up into a polyhedral approximation to the sphere, using particular projection to map each face with low distortion."
+// }, {
+//     name: "Trimetric",
+//     description: "The Chamberlin trimetric projection is a map projection where three points are fixed on the globe and the points on the sphere are mapped onto a plane by triangulation."
+// }, {
+//     name: "Lenticular",
+//     description: "Lenticular projections are similar to the pseudocylindrics. But while those show straight parallels, in lenticular projections the parallels curve toward the poles."
+// }, {
+//     name: "Retroazimuthal",
+//     description: "Direction to a fixed location B (by the shortest route) corresponds to the direction on the map from A to B."
+// }, {
+//     name: "Polyconic",
+//     description: "Refers to those projections whose parallels are all non-concentric circular arcs, except for a straight equator, and the centers of these circles lie along a central axis. This description applies to projections in equatorial aspect."
+// }, {
+//     name: "Quincuncial",
+//     description: "The name quincuncial refers to this arrangement: the north pole at the center and quarters of the south pole in the corners form a quincunx pattern like the pips on the five face of a traditional die. The projection has the distinctive property that it forms a seamless square tiling of the plane, conformal except at four singular points along the equator."
+// }]
+//   , c0 = [{
+//     name: "Equal-area",
+//     description: "Area measure is conserved everywhere."
+// }, {
+//     name: "Conformal",
+//     description: "Preserves angles locally, implying that local shapes are not distorted and that local scale is constant in all directions from any chosen point."
+// }, {
+//     name: "Compromise",
+//     description: "Neither conformal nor equal-area, but a balance intended to reduce overall distortion."
+// }, {
+//     name: "Equidistant",
+//     description: "All distances from one (or two) points are correct. Other equidistant properties are mentioned in the notes."
+// }, {
+//     name: "Gnomonic",
+//     description: "All great circles are straight lines."
+// }, {
+//     name: "Other",
+//     description: "The projection is neither conformal nor equal-area."
+// }, {
+//     name: "Perspective",
+//     description: "The projection results from viewing the globe from some vantage point."
+// }];
--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,50 +1,15 @@
-// import * as d3 from "d3";
-// import { useMapTools } from "../../hooks/useMapTools";
-// import styles from "./map.module.scss";
-// import { setMapProjection } from "../../utils/setMapProjection";
-// // import { setMapProjection } from "../../utils/setMapProjection";
-
-// export const Carto = () => {
-//   // step 1: load geoJSON and create tooltip
-//   const { mapData } = useMapTools();
-//   const sphere: d3.GeoPermissibleObjects = { type: "Sphere" };
-
-//   // render map only when map data is fully loaded
-//   if (!mapData.loading) {
-//     // step 2: render the regions
-//     // compute a path function based on correct projections that we will use later
-//     const path =
-//       mapData.data && d3.geoPath().projection(setMapProjection(mapData.data));
-
-//     const graticule = d3.geoGraticule();
-
-//     if (!path || path == null) {
-//       return;
-//     }
-//     // for each geoJSON coordinate, compute and pass in the equivalent svg path
-//     const healthRegions = mapData?.data?.features?.map((data, index) => {
-//       return <path className="path" d={path(data) as string} key={index} />;
-//     });
-
-//     return (
-//       <>
-//         <svg width="900px" height="500px">
-//           <path className={styles.sphere} d={path(sphere)} />
-//           <path className={styles.graticule} d={path(graticule)}></path>
-//           {healthRegions}
-//         </svg>
-//       </>
-//     );
-//   } else {
-//     return <h1>Loading...</h1>;
-//   }
-// };
 import React, { useState, useEffect } from "react";
-import { geoNaturalEarth1, geoOrthographic, geoPath } from "d3-geo";
+import {
+  geoNaturalEarth1,
+  geoOrthographic,
+  geoPath,
+  geoGraticule,
+} from "d3-geo";
+import { GeoProjection } from "d3";
 
 export const Carto: React.FC = () => {
-  const [projection, setProjection] = useState<any>(geoNaturalEarth1);
-  const [worldMapData, setWorldMapData] = useState<any>(null);
+  const [projection, setProjection] = useState<GeoProjection>(geoNaturalEarth1);
+  const [worldMapData, setWorldMapData] = useState<unknown>(null);
 
   useEffect(() => {
     const fetchWorldMapData = async () => {
@@ -62,14 +27,15 @@ export const Carto: React.FC = () => {
     fetchWorldMapData();
   }, []);
 
-  const changeProjection = (newProjection: any) => {
+  const changeProjection = (newProjection: () => GeoProjection) => {
     setProjection(newProjection);
   };
 
-  const width = 800;
+  const width = 1000;
   const height = 500;
 
   const pathGenerator = geoPath().projection(projection);
+  const graticule = geoGraticule();
 
   if (!worldMapData) {
     return <div>Loading...</div>;
@@ -84,6 +50,14 @@ export const Carto: React.FC = () => {
           fill="#b3daff"
           stroke="#0059b3"
           strokeWidth={0.5}
+        />
+        <path
+          d={pathGenerator(graticule())}
+          fill="none"
+          stroke="#ccc"
+          strokeWidth={0.5}
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </svg>
       <div>
